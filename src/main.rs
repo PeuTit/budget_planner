b7f55ae10c56d607bd6856307df2c02b56d695c2
Titouan <22703571+PeuTit@users.noreply.github.com>
2024-10-28 13:26:23 +0100

    feat: output splitted months as json instead of regular strings (#6)
    Make use of `serde` & `serde_json` to Serialize (and Deserialize) our
    budget months to Json.
    
    - added `serde` feature to `chrono` library
    - removed methods related to outputing strings to stdin
    - updated readme & fixed typo

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ use clap::Parser;
 fn main() {
     let config = Config::parse();
 
-    let carret = "-".repeat(6);
-    println!("{} Budget Planner! {}", carret, carret);
-
     if let Err(e) = budget_planner::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
